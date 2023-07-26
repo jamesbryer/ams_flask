72c31fe601242b214275c8cc1cb2109a442caa9d
James E. Bryer <jamesbryer098@gmail.com>
2023-07-26 12:38:06 +0100

    d

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-#adding more after / will change location hosted after localhost:5000
-@app.route("/")
-def index():
-    return "Hello, World!"
+from application import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
---
+++ application/__init__.py
@@ -0,0 +1,4 @@
+import application.routes
+from flask import Flask
+
+app = Flask(__name__)
